a0e7d8ce6e375ff958e47a06c3f5c854c266c42d
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2022-08-04 17:52:55 +0530

    Add files via upload

---
+++ monsters-rolodex/src/components/card-list-container/card-list-container.component.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import './card.styles.css'
+
+class CardListContainer extends Component {
+    render () {
+        const {name,id,email} = this.props.monster
+        return (
+            <div className="card-container">
+                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                <h1>{name}</h1>
+                <p>{email}</p>
+            </div>
+        )
+    }
+}
+
+export default CardListContainer
